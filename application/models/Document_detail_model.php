ac72d9f32a9977f1e044d5d23d5277a763bf2e3e
Enny <enny.nuraini06@gmail.com>
2019-04-09 15:18:15 +0700

    Access_token_model kurang tutup kurawal tambah Document_detail_model dan Documents_model

---
+++ application/models/Document_detail_model.php
@@ -0,0 +1,25 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+class Document_detail_model extends MY_Model {
+    public $tableName = "document_detail";
+    public $pkey = "fin_id";
+
+    public function __construct(){
+        parent:: __construct();
+    }
+
+    public function getRules($mode="ADD",$id=0){
+
+        $rules = [];
+
+        $rules[] = [
+            'field' => 'fin_document_id',
+            'label' => 'Document_ID',
+            'rules' => 'required',
+            'errors' => array(
+                'required' => '%s tidak boleh kosong'
+            )
+        ];
+
+        return $rules;
+    }
--- application/models/Documents_model.php
+++ application/models/Documents_model.php
@@ -1,9 +1,9 @@
 <?php
 if(!defined('BASEPATH')) exit('No direct script access allowed');
-class Access_token_model extends MY_Model {
-    public $tableName = "access_token";
-    public $pkey = "fin_id";
-    
+class Documents_model extends MY_Model {
+    public $tableName = "documents";
+    public $pkey = "fin_document_id";
+
     public function __construct(){
         parent:: __construct();
     }
@@ -13,8 +13,8 @@ class Access_token_model extends MY_Model {
         $rules = [];
 
         $rules[] = [
-            'field' => 'fst_reff_source_code',
-            'label' => 'Reff Source Code',
+            'field' => 'fst_name',
+            'label' => 'Name',
             'rules' => 'required',
             'errors' => array(
                 'required' => '%s tidak boleh kosong'
@@ -22,8 +22,8 @@ class Access_token_model extends MY_Model {
         ];
 
         $rules[] = [
-            'field' => 'fst_reff_no',
-            'label' => 'Reff No',
+            'field' => 'fst_source',
+            'label' => 'Source',
             'rules' => 'required',
             'errors' => array(
                 'required' => '%s tidak boleh kosong'
@@ -31,8 +31,8 @@ class Access_token_model extends MY_Model {
         ];
 
         $rules[] => [
-            'field' => 'fst_token',
-            'label' => 'Token',
+            'field' => 'fst_print_scope',
+            'label' => 'Print Scope',
             'rules' => 'required',
             'errors' => array(
                 'required' => '%s tidak boleh kosong'
@@ -40,17 +40,8 @@ class Access_token_model extends MY_Model {
         ];
 
         $rules[] => [
-            'field' => 'fst_session_id',
-            'label' => 'Session ID',
-            'rules' => 'required',
-            'errors' => array(
-                'required' => '%s tidak boleh kosong'
-            )
-        ];
-
-        $rules[] => [
-            'field' => 'fdt_expiration_datetime',
-            'label' => 'Expiration Datetime'
+            'field' => 'fbl_flow_control',
+            'label' => 'Flow Control',
             'rules' => 'required',
             'errors' => array(
                 'required' => '%s tidak boleh kosong'
