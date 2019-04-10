d9f5789595ec82bef960585769269f2e5258a379
Enny <enny.nuraini06@gmail.com>
2019-04-10 17:11:40 +0700

    form department

---
+++ application/views/pages/department/form.php
@@ -0,0 +1,174 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<link rel="stylesheet" href="<?=base_url()?>bower_components/select2/dist/css/select2.min.css">
+<link rel="stylesheet" href="<?=base_url()?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+
+<style type="text/css">
+	.border-0{
+		border: 0px;
+	}
+	td{
+		padding: 2px; !important 		
+	}
+</style>
+
+<section class="content-header">
+	<h1>Department<small>Form</small></h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li><a href="#">Departments</a></li>
+		<li><a href="<?=site_url()?>pages/department">Department</a></li>
+		<li class="active title"><?=$title?></li>
+	</ol>
+</section>
+
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-info">
+				<div class="box-header with-border">
+				<h3 class="box-title title"><?=$title?></h3>
+			</div>
+            <!-- end box header -->
+
+            <!-- form start -->
+            <form id="frmOrder" class="form-horizontal">				
+				<div class="box-body">
+					<input type="hidden" name = "<?=$this->security->get_csrf_token_name()?>" value="<?=$this->security->get_csrf_hash()?>">					
+					<input type="hidden" id="frm-mode" value="<?=$mode?>">
+
+					<div class="form-group">
+						<label for="fin_department_id" class="col-md-2 control-label"><?=lang("Department ID #")?></label>
+						<div class="col-md-4">
+							<input type="text" class="form-control" id="fin_department_id" placeholder="Department ID" name="fin_department_id" value="<?=$fin_departmen_id?>">
+							<div id="fin_department_id_err" class="text-danger"></div>
+						</div>
+					</div>
+
+					<div class="form-group">
+                    <label for="fst_department_name" class="col-sm-2 control-label"><?=lang("Department Name *")?></label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="fst_department_name" placeholder="Department Name" name="fst_department_name">
+							<div id="fst_department_name_err" class="text-danger"></div>
+						</div>
+					</div>
+                </div>
+				<!-- end box body -->
+
+                <div class="box-footer">
+                    <a id="btnSubmitAjax" href="#" class="btn btn-primary">Save Ajax</a>
+                </div>
+                <!-- end box-footer -->
+            </form>
+        </div>
+    </div>
+</section>
+
+<script type="text/javascript">
+    $(function(){
+        <?php if($mode == "EDIT"){?>
+			init_form($("#fin_department_id").val());
+		<?php } ?>
+
+        $("#btnSubmitAjax").click(function(event){
+			event.preventDefault();
+			data = new FormData($("#frmDepartment")[0]);
+
+			mode = $("#frm-mode").val();
+			if (mode == "ADD"){
+				url =  "<?= site_url() ?>department/ajx_add_save";
+			}else{
+				url =  "<?= site_url() ?>department/ajx_edit_save";
+			}
+
+			$.ajax({
+				type: "POST",
+				enctype: 'multipart/form-data',
+				url: url,
+				data: data,
+				processData: false,
+				contentType: false,
+				cache: false,
+				timeout: 600000,
+				success: function (resp) {	
+					if (resp.message != "")	{
+						$.alert({
+							title: 'Message',
+							content: resp.message,
+							confirm: function(){
+								//alert('the department clicked yes');
+							}
+						});
+					}
+
+					if(resp.status == "VALIDATION_FORM_FAILED" ){
+						//Show Error
+						errors = resp.data;
+						for (key in errors) {
+							$("#"+key+"_err").html(errors[key]);
+						}
+					}else if(resp.status == "SUCCESS") {
+						data = resp.data;
+						$("#fin_department_id").val(data.insert_id);
+
+						//Clear all previous error
+						$(".text-danger").html("");
+
+						// Change to Edit mode
+						$("#frm-mode").val("EDIT");  //ADD|EDIT
+
+						$('#fst_department_name').prop('readonly', true);
+						$("#tabs-department-detail").show();
+						console.log(data.data_image);
+					}
+				},
+				error: function (e) {
+					$("#result").text(e.responseText);
+					console.log("ERROR : ", e);
+					$("#btnSubmit").prop("disabled", false);
+				}
+			});
+		});
+
+		$(".datepicker").datepicker({
+			format:"yyyy-mm-dd"
+		});
+    })
+
+	function init_form(fin_department_id){
+		//alert("Init Form");
+		var url = "<?= site_url() ?>department/fetch_data/" + <?= $fin_department_id ?>;
+		$.ajax({
+			type: "GET",
+			url: url,
+			success: function (resp) {	
+				console.log(resp.department);
+				
+				$.each(resp.department, function(name, val){
+					var $el = $('[name="'+name+'"]'),
+					type = $el.attr('type');
+					switch(type){
+						case 'checkbox':
+							$el.attr('checked', 'checked');
+							break;
+						case 'radio':
+							$el.filter('[value="'+val+'"]').attr('checked', 'checked');
+							break;
+						default:
+							$el.val(val);
+					}
+				});
+			},
+
+			error: function (e) {
+				$("#result").text(e.responseText);
+				console.log("ERROR : ", e);
+			}
+		});
+	}
+</script>
+
+<!-- DataTables -->
+<script src="<?=base_url()?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?=base_url()?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
