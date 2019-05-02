c7c895011856d916f830cd1b22be50c1f7d26c43
qsystem-online <devibong@qsystem-online.com>
2019-05-02 09:27:53 +0700

    a

--- index.php
+++ index.php
@@ -98,11 +98,8 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-<<<<<<< .mine
+
 	$system_path = 'E:\codeigniter\CI3.1.10\system';
-=======
-	$system_path = 'C:\CI3.1.10\system';
->>>>>>> .theirs
 
 /*
  *---------------------------------------------------------------
