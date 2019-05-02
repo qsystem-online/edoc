5bd10cadf069851f34429f335d1e5abd45632b85
Bahroni <m.bahroni86@gmail.com>
2019-05-02 15:05:05 +0700

    roni 20190502 15:04 sync index + contans

--- application/config/constants.php
+++ application/config/constants.php
@@ -20,7 +20,7 @@ defined('SHOW_DEBUG_BACKTRACE') OR define('SHOW_DEBUG_BACKTRACE', TRUE);
 |
 | These prefs are used when checking and setting modes when working
 | with the file system.  The defaults are fine on servers with proper
-| security, but you may wish (or even need) to change the values in
+| security, but you may wish (or even need) to change XX the values in
 | certain environments (Apache running a separate process for each
 | user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
 | always be used to set the mode correctly.
