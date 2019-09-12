cf33e43af41790f192e25dc3d0fba1f99c1e3b87
qsystem-online <devibong@qsystem-online.com>
2019-09-12 12:08:31 +0700

    devibong 20190912 12:08

--- application/libraries/Datatables.php
+++ application/libraries/Datatables.php
@@ -19,7 +19,7 @@ class Datatables
 	public function __construct()
 	{
 		$this->CI = &get_instance();
-		$this->db = $this->CI->load->database('default', TRUE);
+		$this->db = $this->CI->db;//$this->CI->load->database('default', TRUE);
 	}
 
 
