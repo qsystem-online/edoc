02d5d2e12c3c982748d35f4beef7f22ee458e516
Enny <enny.nuraini06@gmail.com>
2019-09-19 10:43:33 +0700

    enny06 2019/09/19 10.43 update dashboard model

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -48,9 +48,11 @@ class Dashboard_model extends CI_Model {
     }
 
     public function getTotalRejected(){
-        $ssql = "select count(*) as ttl_rejected from documents where fin_insert_id = ? and fst_active = 'R' and fbl_flow_completed = false";
-        $rs = $this->db->query($ssql,[$this->aauth->get_user_id()]);
-        $rw = $rs->row();
+        $ssql = "select count(*) as ttl_rejected from document_flow_control a
+            left join documents b on a.fin_document_id = b.fin_document_id 
+            where a.fst_control_status = 'RJ' and b.fst_active = 'A'";
+        $qr = $this->db->query($ssql,[$this->aauth->get_user_id()]);
+        $rw = $qr->row();
         return $rw->ttl_rejected;
     }
 }
